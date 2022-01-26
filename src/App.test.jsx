6dae647ecacd1e42b46bf6620a318f3ae322a57c
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-01-26 15:37:56 -0800

    got tests to pass

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,15 +3,15 @@
 
 import { screen, render } from '@testing-library/react'
 // 🚨
-// import rest
-// import setupServer
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 import App from './App'
 
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -20,12 +20,22 @@ const user = {
 }
 
 // 🚨 Create your server
+const server = setupServer(
+  rest.get(`https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users`, (req, res, ctx) => {
+    const select = req.url.searchParams.get('select')
+
+    if (select === '*') {
+      return res(ctx.json([user]))
+    }
+    return res(ctx.status(500))
+  })
+)
 
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen())
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.listen())
 
 test('Should render the header', async () => {
   render(<App />)
@@ -53,6 +63,16 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
   }
 
   // 🚨 Use the server to change the response for this test
+  server.use(
+    rest.get(`https://uzgiamkrbapxufnwdrja.supabase.co/rest/v1/users`, (req, res, ctx) => {
+      const select = req.url.searchParams.get('select')
+
+      if (select === '*') {
+        return res(ctx.json([sasuke]))
+      }
+      return res(ctx.status(500))
+    })
+  )
 
   render(<App />)
 
